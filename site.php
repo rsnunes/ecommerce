6b464e8c81775d25446295cf6601a76afca33d08
Renato Nunes <r.silveira.nunes14@gmail.com>
2020-12-06 19:18:03 -0300

    categorias vs produtos

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 
 use \Rsnunes\Page;
 use \Rsnunes\Model\Product;
+use \Rsnunes\Model\Category;
 
 $app->get('/', function() {
     $products = Product::listAll();
@@ -10,6 +11,21 @@ $app->get('/', function() {
 
     $page->setTpl("index", [
         "products"=>Product::checkList($products)
+    ]);
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new Page();
+
+    $page->setTpl("category", [
+        "category"=>$category->getValues(),
+        "products"=>Product::checkList($category->getProducts())
     ]);
 
 });
