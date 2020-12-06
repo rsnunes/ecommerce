c10b84e00294ec732a4400d54c52b627e2f71a0f
Renato Nunes <r.silveira.nunes14@gmail.com>
2020-12-06 11:41:17 -0300

    site - produtos

--- site.php
+++ site.php
@@ -1,12 +1,16 @@
 <?php
 
 use \Rsnunes\Page;
+use \Rsnunes\Model\Product;
 
 $app->get('/', function() {
+    $products = Product::listAll();
 
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index", [
+        "products"=>Product::checkList($products)
+    ]);
 
 });
 
