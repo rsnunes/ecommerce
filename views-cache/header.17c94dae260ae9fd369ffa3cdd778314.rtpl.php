382c2e94ed8e11fdebbaf3ed1d561cece254a133
Renato Nunes <r.silveira.nunes14@gmail.com>
2020-12-13 19:10:14 -0300

    Carrinho produtos

--- views-cache/header.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/header.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -42,7 +42,7 @@
                         <ul>
                             <li><a href="#"><i class="fa fa-user"></i> Minha Conta</a></li>
                             <li><a href="#"><i class="fa fa-heart"></i> Lista de Desejos</a></li>
-                            <li><a href="#"><i class="fa fa-shopping-cart"></i> Meu Carrinho</a></li>
+                            <li><a href="/cart"><i class="fa fa-shopping-cart"></i> Meu Carrinho</a></li>
                             <li><a href="#"><i class="fa fa-lock"></i> Login</a></li>
                         </ul>
                     </div>
